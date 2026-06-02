640c48bb3e8e118f0318af5fb44168d068f58de2
Clueroi <eriicsouzaromeroo@gmail.com>
2026-06-02 20:20:09 -0300

    Feat: Env Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './infra/env/env';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({
+    validate: env => envSchema.parse(env),
+    isGlobal: true
+  }),
+    
+  ],
 })
-export class AppModule {}
+
+export class AppModule { }
