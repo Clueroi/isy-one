8bacd7c95e16d395319da59f2d4e6f7424ec4ca1
Clueroi <eriicsouzaromeroo@gmail.com>
2026-06-03 11:41:01 -0300

    Feat: Added all controllers and Use cases to module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -7,6 +7,12 @@ import { ExecuteScriptController } from './controllers/execute-script.controller
 import { DeleteScriptUseCase } from 'src/domain/scripts/application/use-cases/delete-use-case';
 import { ExecuteScriptUseCase } from 'src/domain/execution/application/use-cases/execute-script-use-case';
 import { ShellModule } from './shell/shell.module';
+import { FetchScriptsController } from './controllers/get-script.controller';
+import { GetScriptController } from './controllers/get-script-by-id.controller';
+import { EditScriptController } from './controllers/update-script.controller';
+import { FetchScriptsUseCase } from 'src/domain/scripts/application/use-cases/find-many-use-case';
+import { GetScriptUseCase } from 'src/domain/scripts/application/use-cases/get-by-id-use-case';
+import { EditScriptUseCase } from 'src/domain/scripts/application/use-cases/update-use-case';
 
 @Module({
   imports: [
@@ -14,14 +20,20 @@ import { ShellModule } from './shell/shell.module';
     ShellModule
   ],
   controllers:[
-    CreateScriptController ,
+    CreateScriptController,
     DeleteScriptController,
+    FetchScriptsController,
+    GetScriptController,
+    EditScriptController,
     ExecuteScriptController,
 
   ],
   providers: [
     CreateScriptUseCase,
     DeleteScriptUseCase,
+    FetchScriptsUseCase,
+    GetScriptUseCase,
+    EditScriptUseCase,
     ExecuteScriptUseCase,
   ]
 
